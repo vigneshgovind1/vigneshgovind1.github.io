7c5fe3a6be7deb906413a4bf638d46476829d8e5
vignesh <vignesh@iamdave.ai>
2023-11-03 13:08:26 +0530

    some final changes

--- Babylon.js
+++ Babylon.js
@@ -8,13 +8,9 @@ window.addEventListener('DOMContentLoaded', function () {
     var box = BABYLON.MeshBuilder.CreateBox('box', { size: 2 }, scene);
     box.position = new BABYLON.Vector3(0, 0, 0);
 
-    // Function to handle traditional joystick input (arrow keys)
-    function handleJoystickInput(joystickValue) {
-        // You can use joystickValue to determine the direction and magnitude of the input.
-        // For example, if joystickValue is positive, move the box right; if negative, move it left.
-        box.position.x += joystickValue * 0.1; // Adjust the speed as needed.
-    }
 
+    /*
+    //FOR PC WEB ONLY
     // Event listener for key presses (arrow keys)
     window.addEventListener('keydown', function (event) {
         if (event.key === 'ArrowLeft') {
@@ -23,26 +19,22 @@ window.addEventListener('DOMContentLoaded', function () {
             box.position.x += 0.1; // Move the box to the right
         }
     });
+    */
 
     // Function to handle gamepad input (physical joystick)
     function handleGamepadInput(gamepad) {
         // Access gamepad axes
         var axes = gamepad.axes;
-
-        // Check the left stick's horizontal axis (index 0) for left and right movement
         var leftStickX = axes[0];
 
-        // Define a movement speed
-        var movementSpeed = 0.01; // Adjust the speed as needed
+        var movementSpeed = 0.05; 
 
         // Check if the left stick is pushed to the left
         if (leftStickX < -0.2) {
-            // Move the box to the left
             box.position.x -= movementSpeed;
         }
         // Check if the left stick is pushed to the right
         else if (leftStickX > 0.2) {
-            // Move the box to the right
             box.position.x += movementSpeed;
         }
     }
@@ -51,12 +43,11 @@ window.addEventListener('DOMContentLoaded', function () {
     window.addEventListener('gamepadconnected', function (event) {
         var gamepad = event.gamepad;
 
-        // Start handling gamepad input
         handleGamepadInput(gamepad);
     });
 
     engine.runRenderLoop(function () {
-        // Call the handleGamepadInput function here
+        //Find and loop gamepads functions here
         var gamepads = navigator.getGamepads();
         if (gamepads[0]) {
             handleGamepadInput(gamepads[0]);
